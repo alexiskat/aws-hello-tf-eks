cefca2e28de90661e3ed89f19fbf26ccd7745b5f
john.katsavras <alexis@example.com>
2022-03-29 10:37:43 +0100

    test gh actions sec encypt fmt

--- infra/tf/02-module/eks/main.tf
+++ infra/tf/02-module/eks/main.tf
@@ -11,15 +11,14 @@ module "kms_key" {
   tags                    = var.tags
 }
 module "dynamodb_terraform_state_lock" {
-  source                         = "../../03-resource/database/dynamodb"
-  name                           = local.dynamodb_name
-  read_capacity                  = var.read_capacity
-  write_capacity                 = var.write_capacity
-  hash_key                       = var.hash_key
-  attributes                     = var.attributes
-  server_side_encryption_enabled = var.server_side_encryption_enabled
-  billing_mode                   = "PROVISIONED"
-  tags                           = var.tags
-
+  source                             = "../../03-resource/database/dynamodb"
+  name                               = local.dynamodb_name
+  read_capacity                      = var.read_capacity
+  write_capacity                     = var.write_capacity
+  hash_key                           = var.hash_key
+  attributes                         = var.attributes
+  server_side_encryption_enabled     = var.server_side_encryption_enabled
+  billing_mode                       = "PROVISIONED"
+  tags                               = var.tags
   server_side_encryption_kms_key_arn = module.kms_key.alias_arn
 }
