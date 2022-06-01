a54dfcc8ee852ef0859228a53693601688da39d6
john.katsavras <alexis@example.com>
2022-06-01 16:34:03 +0100

    trying to get k8s workflow working 12

--- infra/tf/03-resource/process/eks-cluster/main.tf
+++ infra/tf/03-resource/process/eks-cluster/main.tf
@@ -9,7 +9,19 @@ resource "aws_eks_cluster" "this" {
     subnet_ids              = var.subnet_ids
     endpoint_private_access = true
     endpoint_public_access  = true #Make the API server access via internet. Can be to false but need bastion
-    public_access_cidrs     = ["82.24.103.68/32"]
+    #public_access_cidrs     = ["82.24.103.68/32"]
+    public_access_cidrs     = [
+      "13.0.0.0/8",
+      "20.0.0.0/8",
+      "52.0.0.0/8",
+      "65.0.0.0/8",
+      "104.0.0.0/8",
+      "131.0.0.0/8",
+      "157.0.0.0/8",
+      "168.0.0.0/8",
+      "191.0.0.0/8",
+      "199.0.0.0/8"
+      ]
   }
 
   tags = var.tags
